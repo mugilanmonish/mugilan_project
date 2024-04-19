6ccbc9ab63d3a432ed519829a317c154e71ec8b7
mugilanmonish <mugilanmonish@gmail.com>
2024-04-19 17:27:25 +0530

    adad

--- src/test/java/batchexecution/HelpPage.java
+++ src/test/java/batchexecution/HelpPage.java
@@ -1,10 +1,13 @@
 package batchexecution;
 
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 @Test
 public class HelpPage {
 	public void print() {
 		System.out.println("Hi");
+		System.out.println("Bye");
+		Reporter.log("Hi---------------------------Bye");
 	}
 }
